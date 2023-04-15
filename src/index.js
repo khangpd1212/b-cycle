1e3d40b96372db847ef502b4d466013bfcd89c92
Nhut Pham <nhutpd@dgroup.co>
2023-04-15 17:16:58 +0700

    revert

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
 import "../src/assets/fonts/SFSports.ttf";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<React.StrictMode>
-  <App />
-</React.StrictMode>);
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
